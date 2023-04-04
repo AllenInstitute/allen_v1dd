f151c10fe8dcc59c6d6043ad8e388807290114a6
Chase King <iamchaseking@gmail.com>
2023-04-04 16:32:36 -0700

    Remove test mode from stim analysis script

--- scripts/stim_analysis_isilon.sh
+++ scripts/stim_analysis_isilon.sh
@@ -5,7 +5,7 @@ OUTPUT_FILE="~/v1dd_stim_analyses"
 
 echo "Stimulus analysis output file: $OUTPUT_FILE"
 echo "Running stimulus analyses on screen \"$SCREEN_NAME\"..."
-screen -dmS $SCREEN_NAME python data_processing/run_stimulus_analyses.py --test_mode isilon $OUTPUT_FILE --debug
+screen -dmS $SCREEN_NAME python data_processing/run_stimulus_analyses.py isilon $OUTPUT_FILE --debug
 echo
 echo "Type the following to enter screen:"
 echo "  screen -r $SCREEN_NAME"
