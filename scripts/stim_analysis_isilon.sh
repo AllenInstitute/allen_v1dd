d7b4d7bd2265263edf42523521dbccf7c861b1ff
Chase King <iamchaseking@gmail.com>
2023-04-05 10:16:07 -0700

    Stim analysis fix

--- scripts/stim_analysis_isilon.sh
+++ scripts/stim_analysis_isilon.sh
@@ -4,10 +4,14 @@ SCREEN_NAME="stim_analysis"
 OUTPUT_FILE="/home/chase/v1dd_stim_analyses"
 LOG_FILE="/home/chase/v1dd_stim_analyses_most_recent_log.txt"
 
+eval "$(conda shell.bash hook)"
+conda activate allen_v1dd
+
 echo "Stimulus analysis output file: $OUTPUT_FILE"
 echo "Running stimulus analyses on screen \"$SCREEN_NAME\""
-conda activate allen_v1dd
+
 screen -dmS $SCREEN_NAME `python data_processing/run_stimulus_analyses.py isilon $OUTPUT_FILE --debug |& tee $LOG_FILE`
+
 echo
 echo "Type the following to enter screen:"
 echo "  screen -r $SCREEN_NAME"
