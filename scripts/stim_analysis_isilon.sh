cfe01c7822f4a1462c138e85bfd0801c54198f24
Chase King <iamchaseking@gmail.com>
2023-04-05 10:07:27 -0700

    Add backticks to command in sh script

--- scripts/stim_analysis_isilon.sh
+++ scripts/stim_analysis_isilon.sh
@@ -6,7 +6,7 @@ LOG_FILE="/Homes/chase/v1dd_stim_analyses_most_recent_log.txt"
 
 echo "Stimulus analysis output file: $OUTPUT_FILE"
 echo "Running stimulus analyses on screen \"$SCREEN_NAME\""
-screen -dmS $SCREEN_NAME python data_processing/run_stimulus_analyses.py isilon $OUTPUT_FILE --debug |& tee $LOG_FILE
+screen -dmS $SCREEN_NAME `python data_processing/run_stimulus_analyses.py isilon $OUTPUT_FILE --debug |& tee $LOG_FILE`
 echo
 echo "Type the following to enter screen:"
 echo "  screen -r $SCREEN_NAME"
