6abde7013695ab420956efb00044b8c4ee2c6c72
Chase King <iamchaseking@gmail.com>
2023-04-14 23:46:52 +0200

    Fix file path in script echo

--- scripts/stim_analysis_isilon_screen.sh
+++ scripts/stim_analysis_isilon_screen.sh
@@ -12,6 +12,6 @@ screen -dmS $SCREEN_NAME bash -c "./stim_analysis_isilon.sh; exec bash"
 
 echo
 echo "  ENTER SCREEN:  screen -r $SCREEN_NAME"
-echo "  SYNC LOCALLY:  rsync -av --progress allen:/home/chase/v1dd_stim_analyses_DATE ~/Desktop/"
+echo "  SYNC LOCALLY:  rsync -av --progress allen:/mnt/drive2/chase/v1dd/v1dd_stim_analyses_DATE ~/Desktop/"
 # echo "  less $LOG_FILE                  to view log file"
 echo
